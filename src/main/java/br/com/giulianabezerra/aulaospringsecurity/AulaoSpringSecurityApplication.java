0679ab5382cf8f904e9a5ffecc2164852e455c0f
Tiago de Oliveira Villalva <tiagodeoliveiravillalva@gmail.com>
2025-01-04 15:34:32 -0300

    feat(auth): implement OAuth2 login with Google authentication

---
+++ src/main/java/br/com/giulianabezerra/aulaospringsecurity/AulaoSpringSecurityApplication.java
@@ -0,0 +1,14 @@
+package br.com.giulianabezerra.aulaospringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaoSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaoSpringSecurityApplication.class, args);
+	}
+
+}
+
